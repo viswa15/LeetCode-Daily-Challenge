994f4c426fb6227e0cecdc231a245e8be68d8964
viswa15 <viswatejatummala@gmail.com>
2025-09-03 16:31:15 +0530

    Sept 2

--- find-the-number-of-ways-to-place-people-I.py
+++ find-the-number-of-ways-to-place-people-I.py
@@ -40,3 +40,26 @@ points[i].length == 2
 0 <= points[i][0], points[i][1] <= 50
 All points[i] are distinct.
 '''
+
+class Solution:
+    def numberOfPairs(self, points: List[List[int]]) -> int:
+        n = len(points)
+        res = 0
+
+        for i in range(n):
+            ax,ay = points[i]
+            for j in range(n):
+                if i == j:continue
+                bx,by = points[j]
+                if not (ax <= bx and ay >= by):continue
+
+                skip = False
+                for k in range(n):
+                    if k == i or k == j:continue
+                    cx,cy = points[k]
+                    if (cx >= ax and cx <= bx) and (cy <= ay and cy >= by):
+                        skip = True
+                        break
+                
+                if not skip:res+=1
+        return res
